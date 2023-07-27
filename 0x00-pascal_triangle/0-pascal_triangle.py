57461d1ae42614ab0ed5d65d6ec9e6829d45470f
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-07-27 12:37:57 +0100

    Updated to obey pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 0-pascal_triangle.py
 """
 
+
 def pascal_triangle(n):
     if n < 1:
         return []
@@ -11,11 +12,13 @@ def pascal_triangle(n):
 
     for row_number in range(n):
         current_row = []
-        current_row.append(1);
+        current_row.append(1)
 
         for i in range(1, row_number):
-            value = triangle[row_number - 1][i - 1] + triangle[row_number - 1][i]
-            current_row.append(value)
+            first_num = triangle[row_number - 1][i - 1]
+            second_num = triangle[row_number - 1][i]
+            summation = first_num + second_num
+            current_row.append(summation)
 
         if row_number > 0:
             current_row.append(1)
