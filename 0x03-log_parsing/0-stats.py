0e0c4549e2f47f6475594635512bad852ab8f882
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-08-17 01:23:49 +0100

    Obeyed pep8

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
 """
 Log parsing
+
+Script that reads stdin line by line and computes metrics:
+
+    Input format:
+        <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+        <status code> <file size>
+        (if the format is not this one, the line must be skipped)
+
+    After every 10 lines and/or a keyboard interruption (CTRL + C),
+    print these statistics from the beginning:
+        Total file size: File size: <total size>
+        where <total size> is the sum of all previous <file size>
+        (see input format above)
+        Number of lines by status code:
+            possible status code:
+                200, 301, 400, 401, 403, 404, 405 and 500
+            if a status code doesn’t appear or is not an integer,
+            don’t print anything for this status code
+
+            format: <status code>: <number>
+            status codes should be printed in ascending order
 """
 
 import sys
@@ -17,12 +38,6 @@ count: int = 0
 def print_stats(stats: dict, total: int) -> None:
     """
     print_stats function
-
-    Format:
-    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-
-    E.g:
-    4.21.109.220 - [2023-08-17 01:06:01.445472] "GET /projects/260 HTTP/1.1" 500 813
     """
     print("File size: {}".format(total))
     for key, value in sorted(stats.items()):
