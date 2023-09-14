e55b4a7a7c881214c74c5680445f95185b3fca2d
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-14 21:46:18 +0000

    Wrote a function that solves the 2D matrix interview question

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+def rotate_2d_matrix(matrix):
+    reverse = matrix[::-1]
+    for index, value in enumerate(reverse):
+        matrix.append([item[index] for item in reverse])
+    for i in range(len(reverse)):
+        del matrix[0]
