a8f9af9485fa0d4c2a3976655d0aab3ef4850b93
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-21 05:10:29 +0000

    Solved making change interview question

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 """ Make change module"""
 
+
 def makeChange(coins, total):
     '''Makes change'''
+
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    count = 0
+    coins.sort()
+    coins.reverse()
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            count += 1
+        if (total == 0):
+            return count
+    return -1
