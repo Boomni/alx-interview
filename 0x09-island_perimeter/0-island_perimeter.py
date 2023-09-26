dbe3e07074152899e82901ec6ab107e7d826996b
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-26 08:27:19 +0000

    Updated my perimey

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,10 +4,16 @@
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
 
-    squares = 0
+    perimeter = 0
 
-    for lists in grid:
-        for square in lists:
-            if square == 1:
-                squares += 1
-    return 2 * (1 + squares)
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
